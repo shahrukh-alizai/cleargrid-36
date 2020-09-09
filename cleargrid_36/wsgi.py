47889fce4f49639b27a9f015025fa3645662862d
Shah Rukh Khan <shah@crowdbotics.com>
2020-09-09 07:00:29 +0000

    Update models

--- cleargrid_36/wsgi.py
+++ cleargrid_36/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cleargrid_36.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cleargrid_36.settings")
 
 application = get_wsgi_application()
